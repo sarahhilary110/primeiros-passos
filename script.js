e316b8f37d2b2015f9533d98a30014e7c171d707
sarah hilary pereira <143519725+sarahhilary110@users.noreply.github.com>
2025-02-28 12:18:23 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById(opcoes-acessibilidade)
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+    
     const aumentaFonteBotao document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    dimunuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
